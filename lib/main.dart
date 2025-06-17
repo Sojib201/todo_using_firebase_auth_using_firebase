4c542066a34cf83feb7c642f070a4721fa9aa1c0
Md Sojib Molla <sojib.cse3729@gmail.com>
2025-06-17 10:55:39 +0600

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:to_do_with_firebase/login_screen.dart';
 
 import 'auth_gate.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
